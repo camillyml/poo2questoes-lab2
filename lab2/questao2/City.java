c7dbbe6455c99ca976339dd715a401bbc95ae833
Camilly Cruvinel <167707119+camillyml@users.noreply.github.com>
2024-09-08 21:42:39 -0300

    Update City.java

---
+++ lab2/questao2/City.java
@@ -0,0 +1,15 @@
+package lab2.questao2;
+
+class City implements IVehicle {
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void drive() {
+    }
+
+    @Override
+    public void stop() {
+    }
+}
