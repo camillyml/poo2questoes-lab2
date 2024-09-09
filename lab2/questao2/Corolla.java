87a2b8e6d756f7a58ac7c9c0bcbcba6fd0e2312c
Camilly Cruvinel <167707119+camillyml@users.noreply.github.com>
2024-09-08 21:42:52 -0300

    Update Corolla.java

---
+++ lab2/questao2/Corolla.java
@@ -0,0 +1,15 @@
+package lab2.questao2;
+
+class Corolla implements IVehicle {
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void drive() {
+    }
+
+    @Override
+    public void stop() {
+    }
+}
